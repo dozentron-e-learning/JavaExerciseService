1ddbbd952bf46b1eb4745071dedf3ff3d551d315
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-03 21:10:58 +0200

    add test with failing submission

--- spec/jobs/execute_exercise_job_spec.rb
+++ spec/jobs/execute_exercise_job_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ExecuteExerciseJob, type: :job do
     it 'it runs in general and produces test results' do
       exercise_id = 1
       submission_id = 2
-      
+
       submission_download_url = "http://localhost:3001/api/v1/submission/#{submission_id}/download"
       exercise_test_download_url = "http://localhost:3000/api/v1/exercise/#{exercise_id}/download"
       exercise_hidden_test_download_url = "http://localhost:3000/api/v1/exercise/#{exercise_id}/download_hidden"
@@ -29,7 +29,37 @@ RSpec.describe ExecuteExerciseJob, type: :job do
       expect(WebMock).to have_requested(:get, exercise_hidden_test_download_url).once
 
       expect(WebMock).to have_requested(:post, result_url)
-                             #.with(body: hash_including(exercise_id: exercise_id, submission_id: submission_id))
+        #.with(body: hash_including(exercise_id: exercise_id, submission_id: submission_id))
+    end
+
+    it 'it can handle test failures and uploaded Junit tests' do
+      exercise_id = 1
+      submission_id = 2
+
+      submission_download_url = "http://localhost:3001/api/v1/submission/#{submission_id}/download"
+      exercise_test_download_url = "http://localhost:3000/api/v1/exercise/#{exercise_id}/download"
+      exercise_hidden_test_download_url = "http://localhost:3000/api/v1/exercise/#{exercise_id}/download_hidden"
+
+      result_url = 'http://localhost:3002/results'
+
+      submission_response = File.new(Rails.root.join('spec', 'resources', 'raw_curl_responses', 'submission_failure_download'))
+      test_response = File.new(Rails.root.join('spec', 'resources', 'raw_curl_responses', 'test_download'))
+      hidden_test_response = File.new(Rails.root.join('spec', 'resources', 'raw_curl_responses', 'hidden_test_download'))
+
+      stub_request(:get, submission_download_url).to_return(submission_response)
+      stub_request(:get, exercise_test_download_url).to_return(test_response)
+      stub_request(:get, exercise_hidden_test_download_url).to_return(hidden_test_response)
+
+      stub_request(:post, result_url).to_return(status: 200)
+
+      ExecuteExerciseJob.perform exercise_id, submission_id, ''
+
+      expect(WebMock).to have_requested(:get, submission_download_url).once
+      expect(WebMock).to have_requested(:get, exercise_test_download_url).once
+      expect(WebMock).to have_requested(:get, exercise_hidden_test_download_url).once
+
+      expect(WebMock).to have_requested(:post, result_url)
+      #.with(body: hash_including(exercise_id: exercise_id, submission_id: submission_id))
     end
   end
 end
