4035abf50b5a8376e4370f46a5de92ad664a4fe2
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-07 13:37:28 +0200

    refactor validate_exercise_job to allow for easier testing and better readability

--- app/jobs/validate_exercise_job.rb
+++ app/jobs/validate_exercise_job.rb
@@ -11,21 +11,26 @@ class ValidateExerciseJob
   EXERCISE_UPDATE_URL = "#{Rails.configuration.service_urls.exercise_service}/api/v1/exercises"
 
   def self.perform(exercise_id, token)
-    general_validation_error = nil
-    general_validation_error_details = nil
-
     prepare_execution_environment
 
     download_exercise(token, exercise_id, TEST_FILENAME)
     download_exercise_hidden(token, exercise_id, HIDDEN_TEST_FILENAME)
     download_exercise_stub(token, exercise_id, STUB_FILENAME)
 
-    stub_validation_error = validate_jar execution_directory(STUB_FILENAME), execution_directory('stub')
-    test_validation_error = validate_jar execution_directory(TEST_FILENAME), execution_directory('test')
-    hidden_test_validation_error = validate_jar execution_directory(HIDDEN_TEST_FILENAME), execution_directory('hidden_test')
+    payload = validate_exercise(execution_directory(TEST_FILENAME), execution_directory(HIDDEN_TEST_FILENAME), execution_directory(STUB_FILENAME))
+    RestClient.patch "#{EXERCISE_UPDATE_URL}/#{exercise_id}", payload
+  end
 
-    unzip_file execution_directory(TEST_FILENAME), execution_directory('src', 'test', 'java')
-    unzip_file execution_directory(HIDDEN_TEST_FILENAME), execution_directory('src', 'test', 'java')
+  def self.validate_exercise(test_path, hidden_test_path, stub_path)
+    general_validation_error = nil
+    general_validation_error_details = nil
+
+    test_validation_error = validate_jar test_path, execution_directory('test')
+    hidden_test_validation_error = validate_jar hidden_test_path, execution_directory('hidden_test')
+    stub_validation_error = validate_jar stub_path, execution_directory('stub')
+
+    unzip_file test_path, execution_directory('src', 'test', 'java')
+    unzip_file hidden_test_path, execution_directory('src', 'test', 'java')
     _, output, error, pid = run_gradle_task 'compileTestJava'
     exit_status = pid.value
 
@@ -34,7 +39,6 @@ class ValidateExerciseJob
       general_validation_error_details = output.read + "\n" + error.read
     else
       unzip_file execution_directory(STUB_FILENAME), execution_directory('src', 'test', 'java')
-
       _, output, error, pid = run_gradle_task 'compileTestJava'
       exit_status = pid.value
 
@@ -44,22 +48,21 @@ class ValidateExerciseJob
       end
     end
 
-    validation_succeeded = test_validation_error.nil? &&
-        hidden_test_validation_error.nil? &&
-        stub_validation_error.nil? &&
-        general_validation_error.nil? &&
-        general_validation_error_details.nil?
+    validation_succeeded = test_validation_error.nil?
+    validation_succeeded &&= hidden_test_validation_error.nil?
+    validation_succeeded &&= stub_validation_error.nil?
+    validation_succeeded &&= general_validation_error.nil?
+    validation_succeeded &&= general_validation_error_details.nil?
 
-    payload = {
+    {
         api_v1_exercise: {
-          validation_status: validation_succeeded ? VALIDATION_SUCCEEDED : VALIDATION_FAILED,
-          test_validation_error: test_validation_error,
-          hidden_test_validation_error: hidden_test_validation_error,
-          stub_validation_error: stub_validation_error,
-          general_validation_error: general_validation_error,
-          general_validation_error_details: general_validation_error_details
+            validation_status: validation_succeeded ? VALIDATION_SUCCEEDED : VALIDATION_FAILED,
+            test_validation_error: test_validation_error,
+            hidden_test_validation_error: hidden_test_validation_error,
+            stub_validation_error: stub_validation_error,
+            general_validation_error: general_validation_error,
+            general_validation_error_details: general_validation_error_details
         }
     }
-    RestClient.patch "#{EXERCISE_UPDATE_URL}/#{exercise_id}", payload
   end
 end
