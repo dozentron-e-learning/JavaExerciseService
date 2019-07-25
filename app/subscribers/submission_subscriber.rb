95a5cafed4454b08795fe39b4d196ec171fc34ae
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-25 16:49:10 +0200

    fix submission subscriber

--- app/subscribers/submission_subscriber.rb
+++ app/subscribers/submission_subscriber.rb
@@ -1,9 +1,11 @@
 class SubmissionSubscriber < ActiveBunny::Subscriber
-  def create(submission)
-    Resque.enqueue ValidateSubmissionJob, submission.exercise_id, submission.id, submission.auth_token
+  def create(submission_json)
+    submission = JSON.parse(submission_json).with_indifferent_access
+    Resque.enqueue ValidateSubmissionJob, submission[:exercise_id], submission[:id], submission[:validation_token]
   end
 
-  def update(submission)
-    Resque.enqueue ExecuteExerciseJob, submission.exercise_id, submission.id, submission.auth_token
+  def update(submission_json)
+    submission = JSON.parse(submission_json).with_indifferent_access
+    Resque.enqueue ExecuteExerciseJob, submission[:exercise_id], submission[:id], submission[:validation_token]
   end
 end
