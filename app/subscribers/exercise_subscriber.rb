46a9e7625eb6f7c5d7aa41dcedcaeb508088ce14
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-28 21:15:19 +0200

    add logging

--- app/subscribers/exercise_subscriber.rb
+++ app/subscribers/exercise_subscriber.rb
@@ -1,6 +1,7 @@
 class ExerciseSubscriber < ActiveBunny::Subscriber
   def create(exercise_json)
     exercise = JSON.parse(exercise_json).with_indifferent_access
+    Rails.logger.info "Accepted new Exercise Validation Job for exercise: #{exercise[:id]}"
     Resque.enqueue ValidateExerciseJob, exercise[:id], exercise[:validation_token]
   end
 end
--- app/subscribers/submission_subscriber.rb
+++ app/subscribers/submission_subscriber.rb
@@ -1,11 +1,13 @@
 class SubmissionSubscriber < ActiveBunny::Subscriber
   def create(submission_json)
     submission = JSON.parse(submission_json).with_indifferent_access
+    Rails.logger.info "Accepted new Submission Validation Job for submission: #{submission[:id]} and exercise: #{submission[:exercise_id]}"
     Resque.enqueue ValidateSubmissionJob, submission[:exercise_id], submission[:id], submission[:validation_token]
   end
 
   def update(submission_json)
     submission = JSON.parse(submission_json).with_indifferent_access
+    Rails.logger.info "Accepted new Execution Job for submission: #{submission[:id]} and exercise: #{submission[:exercise_id]}"
     Resque.enqueue ExecuteExerciseJob, submission[:exercise_id], submission[:id], submission[:validation_token]
   end
 end
